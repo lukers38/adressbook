515894280da1113a27acd2cd814fb80354cc5a2e
Lukas L <lukas.lichtmannecker@stud.hs-bochum.de>
2021-06-18 15:08:13 +0200

    +SQLiteCreateTable

--- app/SQLiteCreateTable.php
+++ app/SQLiteCreateTable.php
@@ -17,22 +17,32 @@ class SQLiteCreateTable{
         $this->pdo = $pdo;
     }
 
-    public function insertSample(){
-        $sql = "INSERT INTO adress(first_name,last_name,city,street,phone)
-                VALUES('Max','Musterman','Musterstadt','Musterstraße','0123 454545') ";
-        $this->pdo->exec($sql);
+    public function insertSample($user_id){
+        $stmt = $this->pdo->prepare("INSERT INTO adress(first_name,last_name,city,street,phone,user_id)
+                VALUES('Max','Musterman','Musterstadt','Musterstraße','0123 454545',:user_id) ");
+        //bind user_id 
+        $stmt->bindValue(":user_id",$user_id,PDO::PARAM_INT);
+        $stmt->execute();
     }
     
     public function createTable(){
-        $sql = "CREATE TABLE IF NOT EXISTS adress (
+        $sql_adress = "CREATE TABLE IF NOT EXISTS adress (
                 adress_id INTEGER PRIMARY KEY,
                 first_name TEXT,
                 last_name TEXT,
                 city TEXT,
                 street TEXT,
                 phone TEXT,
-                edit BOOLEAN DEFAULT 0)";
-        $this->pdo->exec($sql);
+                edit BOOLEAN DEFAULT 0,
+                user_id INTEGER,
+                FOREIGN KEY(user_id) REFERENCES user(user_id))";
+        $sql_users = "CREATE TABLE IF NOT EXISTS user (
+            user_id INTEGER PRIMARY KEY,
+            username TEXT,
+            password TEXT)";
+        $this->pdo->exec($sql_users);
+        $this->pdo->exec($sql_adress);
+
     }
 }
 ?>
